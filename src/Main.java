db6b340e2919629ff43246b7403c26706ce0a902
vedu2811 <vedant28j@gmail.com>
2025-12-09 18:11:50 +0530

    Added : UC1/Ability to create contact

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main{
+    public static void main(String[] args) {
+        Contact c1=new Contact("Vedant","Jalan","Malancha","Kgp","WB","3334445556","abc@gmail.com",123456);
+        System.out.println(c1);
+
+    }
+}
